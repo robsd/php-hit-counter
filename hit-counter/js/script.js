0dcd0a65163819cf4762f4002445954ce542133a
Robert Dixon <79385125+robsd@users.noreply.github.com>
2022-04-17 23:21:02 +0100

    Add files via upload

--- hit-counter/js/script.js
+++ hit-counter/js/script.js
@@ -1,7 +1,5 @@
-uri = window.location.pathname;
-
 request = new XMLHttpRequest();
-request.open('GET', 'logger/logger.php?uri=' + uri);
+request.open('GET', 'hit-counter/counter.php');
 request.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		document.getElementById('hit-count').innerHTML = this.responseText;
